4456391b5a65e8db256ba8fb2783f4c36b0e8dd6
Manohar Mankala <manohar_mankala@anfcorp.com>
2020-03-17 23:43:28 -0400

    readying app for deployment

--- src/index.js
+++ src/index.js
@@ -1,22 +1,34 @@
-const { COLLECTIONS_DATA } = require('./data');
-const { GraphQLServer } = require('graphql-yoga');
-const { resources } = require('./connectors');
-const fetch = require('node-fetch');
+const { COLLECTIONS_DATA } = require("./data");
+const { GraphQLServer } = require("graphql-yoga");
+const { resources } = require("./connectors");
+const fetch = require("node-fetch");
 
 const resolvers = {
   Query: {
     test: () => `This is the test endpoint API for a simple application`,
     collections: () => COLLECTIONS_DATA,
-    collection: (root, { id }) => COLLECTIONS_DATA.find(collection => collection.id === id),
-    getCollectionsByTitle: (root, { title }) => COLLECTIONS_DATA.find(collection => collection.title === title),
-    resources: () => fetch(`https://my-json-server.typicode.com/MankalMR/tech-resource-finder/resources`).then(res => res.json())
+    collection: (root, { id }) =>
+      COLLECTIONS_DATA.find(collection => collection.id === id),
+    getCollectionsByTitle: (root, { title }) =>
+      COLLECTIONS_DATA.find(collection => collection.title === title),
+    resources: () =>
+      fetch(
+        `https://my-json-server.typicode.com/MankalMR/tech-resource-finder/resources`
+      ).then(res => res.json())
     // resources: () => resources().then(res => res.data)
   }
 };
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
+  typeDefs: "./src/schema.graphql",
   resolvers
 });
 
-server.start(() => console.log(`The server is running on http://localhost:4000`));
+const serverOptions = {
+  port: process.env.PORT || 4000,
+  endpoint: "/graphql",
+  playground: "/playground"
+};
+server.start(serverOptions, ({ endpoint }) =>
+  console.log(`The server is running on ${endpoint}`)
+);
